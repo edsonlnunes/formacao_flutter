1cee8472858809159a9b59c2df16da381e7f8b48
Edson Martins <edsonmartins3110@gmail.com>
2023-09-07 17:17:55 -0300

    first commit

---
+++ desafios/fase_3/lib/widgets/search_products.widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SearchProducts extends StatelessWidget {
+  const SearchProducts({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 35,
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.zero,
+          fillColor: Colors.white,
+          filled: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(100),
+            borderSide: BorderSide.none,
+          ),
+          hintText: "Buscar no Mercado Livre",
+          prefixIcon: const Icon(
+            Icons.search,
+          ),
+        ),
+      ),
+    );
+  }
+}
