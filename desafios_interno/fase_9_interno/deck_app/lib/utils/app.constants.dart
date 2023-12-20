0cdbeb5e1fd58c14dda5402f1f342e454413f361
Edson Martins <edsonmartins3110@gmail.com>
2023-12-20 19:48:06 -0300

    add projeto fase 9

---
+++ desafios_interno/fase_9_interno/deck_app/lib/utils/app.constants.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+
+abstract class AppConstants {
+  static const String baseUrl =
+      'https://15c8-2804-4508-5115-fd41-fc9e-4f93-55a8-e4e4.ngrok-free.app/api';
+  static String? token;
+  static Map<String, String> get headerApi => {
+        'authorization': 'Bearer $token',
+      };
+  static final dioOptions = Options(headers: AppConstants.headerApi);
+}
