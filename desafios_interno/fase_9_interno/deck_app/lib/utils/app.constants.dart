8553e5bc8c06bc1039fadbcc43e91639b83a6612
Edson Martins <edsonmartins3110@gmail.com>
2024-02-18 12:32:14 -0300

    aa

--- desafios_interno/fase_9_interno/deck_app/lib/utils/app.constants.dart
+++ desafios_interno/fase_9_interno/deck_app/lib/utils/app.constants.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 abstract class AppConstants {
   static const String baseUrl =
-      'https://3a25-2804-4508-5115-fd41-fc9e-4f93-55a8-e4e4.ngrok-free.app/api';
+      'https://9512-2804-4508-511a-9081-447d-616f-ea1b-5e25.ngrok-free.app/api';
   static String? token;
   static Map<String, String> get headerApi => {
         'authorization': 'Bearer $token',
